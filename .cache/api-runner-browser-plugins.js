c47378a2d1348138557fc7e9e5d01f482f5c175b
Albert Park <albe.park@gmail.com>
2018-12-03 00:28:51 -0800

    removes dirs

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/albert/github/gatsby-typescript-sass-starter/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/albert/github/gatsby-typescript-sass-starter/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
